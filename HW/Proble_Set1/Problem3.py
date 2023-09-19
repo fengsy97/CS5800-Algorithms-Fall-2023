94bb0ceac546f77f4ebf52c6d97fbf9c7ce896b1
TTC <42894515+fengsy97@users.noreply.github.com>
2023-09-19 12:58:19 -0400

    Update Problem3.py

--- HW/Proble_Set1/Problem3.py
+++ HW/Proble_Set1/Problem3.py
@@ -1,12 +1,15 @@
 # in python3
 
 def getevents(nums,t):
+    answer = 0
     print("Input array is:",nums)
     print("Threshold is ",t)
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if(nums[i] > t * nums[j]):
                 print("Excute event E because a[%d] > %d * a[%d]"%(i,t,j))
-nums = [8, 7, 2, 2, 0, 9, 6]
-t = 2
+                answer += 1
+    print("The total number of events is %d"%answer )
+nums = [4, 2, 11, 5, 7, 8]
+t = 1
 getevents(nums,t)
